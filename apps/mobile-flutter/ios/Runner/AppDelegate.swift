6a4ffcd352f6f171f28676af72d0492984efea94
yurivan0813-wq <yurivan0813@gmail.com>
2026-07-14 18:58:49 +0800

    Harden iOS Firebase bootstrap

--- apps/mobile-flutter/ios/Runner/AppDelegate.swift
+++ apps/mobile-flutter/ios/Runner/AppDelegate.swift
@@ -1,12 +1,18 @@
-import Flutter
-import UIKit
 import FirebaseCore
 import FirebaseMessaging
+import Flutter
+import UIKit
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    if FirebaseApp.app() == nil { FirebaseApp.configure() }
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    if FirebaseApp.app() == nil,
+       Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist") != nil {
+      FirebaseApp.configure()
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
